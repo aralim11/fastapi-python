348ccea00750a4d0edbb8c64ac98cc5f3e1fabb7
aralim11 <aralim11@gmail.com>
2025-06-03 00:45:21 +0600

    Working on user create

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI, Depends, status, Response, HTTPException
+from fastapi import FastAPI, Depends, status, HTTPException, Response
 from sqlalchemy.orm import Session
 from schemas.blog import Blog
+from schemas.user import User
 import models
 from database import engine, get_db
 
@@ -8,14 +9,15 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
- 
+## get all blogs
 @app.get("/blog", status_code=status.HTTP_200_OK)
-def getData(response: Response, db: Session = Depends(get_db)):
+def getData(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     if not blogs:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Blog not found")
     return {"data": blogs}
 
+## get blog by ID
 @app.get("/blog/{id}", status_code=status.HTTP_200_OK)
 def show(id: int, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
@@ -31,3 +33,27 @@ def createBlog(blog: Blog, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_blog)
     return {"data": new_blog}
+
+## update blog
+@app.put("/blog/{id}", status_code=status.HTTP_202_ACCEPTED)
+def blogUpdate(id, request: Blog ,db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Blog not found")
+
+    blog.update({'title': request.title, 'description': request.description, 'published': request.published})
+    db.commit()
+    return {"data": "Updated"}
+
+## delete blog
+@app.delete("/blog/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def deleteBlog(id, db: Session = Depends(get_db)):
+    db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+    db.commit()
+    raise HTTPException(status_code=status.HTTP_204_NO_CONTENT, detail="Blog not deleted")
+    
+
+## create user
+@app.post("/user")
+def createUser(request: User):
+    return {"OK"}
--- schemas/blog.py
+++ schemas/blog.py
@@ -1,10 +1,7 @@
 from pydantic import BaseModel
 from typing import Optional
-from datetime import datetime, timezone
 
 class Blog(BaseModel):
     title: str
     description: str
     published: Optional[bool] = True
-    created_at: datetime = datetime.now(timezone.utc)
-    updated_at: datetime = datetime.now(timezone.utc)
